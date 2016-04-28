34145bea934b5e04884c63dbfa43d55c5e40ccf3
Marius Neugebauer <marius@teele.eu>
2016-04-28 19:54:06 +0200

    cards from social media

---
+++ Cards Against Social Media/README.md
@@ -0,0 +1,3 @@
+# Cards Against Social Media
+
+This is not a game or expansion, it is just a collection of cards that people shared on social media.
---
+++ Cards Against Social Media/create_cards.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# run this script in the directory where you have your "white.txt", "black.txt" and the "templates" directory containing the "white.sla" and "black.sla"
+#############
+
+
+#############
+### WHITE ###
+#############
+
+# create dir for the white cards
+mkdir white
+
+# this is the line we replace in the template
+TPL_LINE='<ITEXT FONT="Roboto Bold" FONTSIZE="12" FCOLOR="Black" KERN="-1.5" wordTrack="1" CH="Cards Against Humanity"/>'
+
+# loop through lines in the white.txt
+while read -u 10 LINE; do
+
+	# remove those chars from the string: /\*"|<>:?
+	TEXT="${LINE//[\/\\\*\"\|<>:\?]/_}"
+	# remove "."s at the end
+	TEXT="$( echo "$TEXT" | sed 's/\.*$//' )"
+	
+	# escape chars that don't work with .sla files
+	LINE="${LINE//&/\&amp;}"
+	LINE="${LINE//\"/\&quot;}"
+	LINE="${LINE//</\&lt;}"
+	
+	# escape "|" for sed
+	LINE="${LINE//[\|]/\|}"
+	
+	# this is the line we replace the other line in the template with
+	TPL_NEW_LINE="<ITEXT FONT=\"Roboto Bold\" FONTSIZE=\"18\" FCOLOR=\"Black\" KERN=\"-1.5\" wordTrack=\"1\" CH=\"$LINE\"/>"
+	
+	# write the .sla file
+	cat ./templates/white.sla | sed "s|$TPL_LINE|$TPL_NEW_LINE|" > "./white/$TEXT.sla"
+	
+done 10<white.txt
+
+#############
+### BLACK ###
+#############
+
+# create dir for the black cards
+mkdir black
+
+# this is the line we replace in the template
+TPL_LINE='<ITEXT FONT="Roboto Bold" FONTSIZE="16" FCOLOR="White" KERN="-1.5" wordTrack="1" CH="This is a template for ______________."/>'
+
+# loop through lines in the black.txt
+while read -u 10 LINE; do
+
+	# remove those chars from the string: /\*"|<>:?
+	TEXT="${LINE//[\/\\\*\"\|<>:\?]/_}"
+	# remove "."s at the end
+	TEXT="$( echo "$TEXT" | sed 's/\.*$//' )"
+	
+	# escape chars that don't work with .sla files
+	LINE="${LINE//&/\&amp;}"
+	LINE="${LINE//\"/\&quot;}"
+	LINE="${LINE//</\&lt;}"
+	
+	# escape "|" for sed
+	LINE="${LINE//[\|]/\|}"
+	
+	# this is the line we replace the other line in the template with
+	TPL_NEW_LINE="<ITEXT FONT=\"Roboto Bold\" FONTSIZE=\"18\" FCOLOR=\"White\" KERN=\"-1.5\" wordTrack=\"1\" CH=\"$LINE\"/>"
+	
+	# write the .sla file
+	cat ./templates/black.sla | sed "s|$TPL_LINE|$TPL_NEW_LINE|" > "./black/$TEXT.sla"
+	
+done 10<black.txt
+
